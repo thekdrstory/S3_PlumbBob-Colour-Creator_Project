cac82105704fa45053f6fc460fd5a6238237f184
thekdrstory <82068638+thekdrstory@users.noreply.github.com>
2023-04-14 15:20:09 -0700

    Uses packages instead of xml

--- S3_PlumbBob-Colour-Creator_/MainWindow.xaml.cs
+++ S3_PlumbBob-Colour-Creator_/MainWindow.xaml.cs
@@ -22,6 +22,10 @@ using System.Drawing;
 using Color = System.Drawing.Color;
 using System.Diagnostics;
 using System.IO;
+using s3pi.Interfaces;
+using s3pi.Package;
+using s3pi.Extensions;
+using Microsoft.Win32;
 
 namespace S3_PlumbBob_Colour_Creator_
 {
@@ -33,28 +37,9 @@ namespace S3_PlumbBob_Colour_Creator_
             InitializeComponent();
         }
 
-        // Might change this section to work with GameplayData.package in the future...
-        // I want to make this more efficient and require less steps...
-        private void open_FileExplorer(object sender, RoutedEventArgs e)
-        {
-            // Creates OpenFileDialog and sets to search only .xml files
-            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.DefaultExt = ".xml";
-            dialog.Filter = "XML Files (*.xml)|*.xml";
-
-            // Display OpenFileDialog by calling ShowDialog method
-            var result = dialog.ShowDialog();
-
-            // Get the selected file name and display in Search Path text box...
-            if (result == true)
-            {
-                txtBox_fileDirectoryPath.Text = dialog.FileName;
-            }
-        }
-
+        // This brings up Colourpicker dialog and lets you choose a colour! It assigns that colour to the button!
         private void colour_picker(object sender, RoutedEventArgs e)
         {
-            // This brings up Colourpicker dialog and lets you choose a colour! It assigns that colour to the button!
             Button colourButton = (Button)sender;
             ColorPickerDialog colourPicker = new ColorPickerDialog();
             var result = colourPicker.ShowDialog();
@@ -65,25 +50,54 @@ namespace S3_PlumbBob_Colour_Creator_
             }
         }
 
+        // When you click the generate button, this is called...
         private void generate_rgb(object sender, RoutedEventArgs e)
         {
+            string xmlPath = @"Assets\moodmanager.xml";
+
             // Clears textbox and generates each of the 4 colour's RGB codes and formats it.
             txtBox_rgbCode.Text = "";
             txtBox_rgbCode.Text = GenerateRgb(btn_horrible.Background.ToString()) + ", " + GenerateRgb(btn_bad.Background.ToString()) 
                 + ", " + GenerateRgb(btn_okay.Background.ToString()) + ", " + GenerateRgb(btn_good.Background.ToString());
 
-            // Opens moodmanager(or file) in notepad and edits it's values if applicable...
-            if (!string.IsNullOrEmpty(txtBox_fileDirectoryPath.Text))
+            // Prompts user to save.
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "PACKAGE Files (*.package)|*.package";
+            dialog.Title = "Save Package";
+            var result = dialog.ShowDialog();
+            string newPackageFilePath = "";
+
+            // Opens file directory to save using user inputted name.
+            if (result == true)
             {
-                string fileName = txtBox_fileDirectoryPath.Text;
-                string newLine = "    <kPlumbbobColorRanges value=\"" + txtBox_rgbCode.Text + "\">";
-                lineChanger(newLine, fileName, 70);
-                MessageBox.Show("Added RGB codes to file!");
+                newPackageFilePath = dialog.FileName;
             }
-            else
-            {
-                MessageBox.Show("No file provided.\nGiving rgb codes anyways...");
-            }
+
+            // Calls lineChanger Method below...
+            string newLine = "    <kPlumbbobColorRanges value=\"" + txtBox_rgbCode.Text + "\">";
+            lineChanger(newLine, xmlPath, 70);
+            MessageBox.Show("Added RGB codes to file!");
+
+            // create the package
+            IPackage NewPackage = Package.NewPackage(0);
+
+            // create resource
+            TGIN tgin = new TGIN();
+            tgin.ResType = 0x0333406C;
+            tgin.ResGroup = 0x00000000;
+            tgin.ResInstance = 0x0b8655fc55e8a6a7;
+            IResourceKey rk = (TGIBlock)tgin;
+
+            // Reads the moodmanager xml and adds it to a new package file (created from the save file dialog.
+            FileStream fs = new FileStream(xmlPath, FileMode.OpenOrCreate, FileAccess.Read);
+            MemoryStream ms = new MemoryStream();
+            fs.CopyTo(ms);
+            IResourceIndexEntry irie = NewPackage.AddResource(rk, ms, true);
+            irie.Compressed = 0x5A42;
+
+            // save the package
+            NewPackage.SaveAs(newPackageFilePath);
+
         }
 
         // This converts the 8 digit hex code you get from buttons and changes them to rgb values
@@ -100,7 +114,7 @@ namespace S3_PlumbBob_Colour_Creator_
             return string.Format("{0:N2}, {1:N2}, {2:N2}", sr, sg, sb);
         }
 
-        // This method opens the user chosen .xml file and goes to a certain line (hard-coded to 70) and replaces the entire line
+        // This method adds the new custom colours in the moodmanager xml in the new package.
         static void lineChanger(string newText, string fileName, int lineNumber)
         {
             string[] fileLines = File.ReadAllLines(fileName);
